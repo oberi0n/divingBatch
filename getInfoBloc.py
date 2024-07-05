127514684167f81c90a5caa692b85493229c1598
oberi0n <67629253+oberi0n@users.noreply.github.com>
2024-07-05 19:24:25 +0200

    Fix Error
    Add error capabilities on the absence of scuba tank

--- getInfoBloc.py
+++ getInfoBloc.py
@@ -3,26 +3,29 @@ from bs4 import BeautifulSoup as soup
 
 #Récuperation des infos d'un bloc dans la page TIV de la FFESSM
 def getInfoBloc(fabId, numSerie): 
-	print("Recuperation des infos du bloc ayant pour numéro de serie: " + numSerie)
-	url = "https://tiv.ffessm.fr/InformationsBloc?FabricantId="+fabId+"&NumeroSerie="+numSerie
+	try:
+		print("Recuperation des infos du bloc ayant pour numéro de serie: " + numSerie)
+		url = "https://tiv.ffessm.fr/InformationsBloc?FabricantId="+fabId+"&NumeroSerie="+numSerie
 
-	req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
-	webpage = urlopen(req).read()
-	page_soup = soup(webpage, "html.parser")
+		req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
+		webpage = urlopen(req).read()
+		page_soup = soup(webpage, "html.parser")
 
-	page_soup = page_soup.find("div", {"id": "results"})
-	sub_soup = page_soup.find("div", {"class": "form-group"})
+		page_soup = page_soup.find("div", {"id": "results"})
+		sub_soup = page_soup.find("div", {"class": "form-group"})
 
-	#Récuperation de la date de validité
-	dateValide = sub_soup.find("div", {"class": "col-md-2"}).text.strip()
+		#Récuperation de la date de validité
+		dateValide = sub_soup.find("div", {"class": "col-md-2"}).text.strip()
 
-	#Récuperation du statut
-	statutValide = 'NOK'
-	for sub_heading in sub_soup.find_all("div", {"class": "col-md-4"}):
-		if "alid" in sub_heading.text:
-			statutValide = 'OK'
+		#Récuperation du statut
+		statutValide = 'NOK'
+		for sub_heading in sub_soup.find_all("div", {"class": "col-md-4"}):
+			if "alid" in sub_heading.text:
+				statutValide = 'OK'
 
-	#Récuperation de la date de qualification
-	dateRequalif = page_soup.find("div", {"class": "col-md-3"}).text.strip()
+		#Récuperation de la date de qualification
+		dateRequalif = page_soup.find("div", {"class": "col-md-3"}).text.strip()
 
-	return dateValide + "," + statutValide + "," + dateRequalif
+		return dateValide + "," + statutValide + "," + dateRequalif
+	except:
+		return "error"
--- updateBloc.py
+++ updateBloc.py
@@ -6,19 +6,25 @@ import datetime
 def updateBloc(idEquipment,fabricant,numeroSerie,connection):
 	
 	#Récup des infos du bloc
-	listData = getInfoBloc.getInfoBloc(fabricant, numeroSerie).split(",")
+	testData = getInfoBloc.getInfoBloc(fabricant, numeroSerie)
+	
+	if testData == "error":
+		print("error")
+	else:
+		listData = testData.split(",")
+		#listData = getInfoBloc.getInfoBloc(fabricant, numeroSerie).split(",")
 
-	entretienDate=str(datetime.datetime.strptime(listData[0],"%d/%m/%Y"))
-	status=str(listData[1])
-	requalifDate=str(datetime.datetime.strptime(listData[2],"%d/%m/%Y"))
+		entretienDate=str(datetime.datetime.strptime(listData[0],"%d/%m/%Y"))
+		status=str(listData[1])
+		requalifDate=str(datetime.datetime.strptime(listData[2],"%d/%m/%Y"))
 
-	sql = "update Equipment set dateDernierEntretien = '" + entretienDate + "', dateRequalif = '" + requalifDate + "', statutTIV = '" + status + "' where idEquipment = " + idEquipment 
-	print("Mise à jour des infos du bloc ayant pour numéro de serie: " + numeroSerie)
-	cursor = connection.cursor() 
-	# Exécution sql
-	cursor.execute(sql)  
-	# Commit
-	connection.commit()    
-	print("Database mise à jour pour le bloc n°" + numeroSerie)
+		sql = "update Equipment set dateDernierEntretien = '" + entretienDate + "', dateRequalif = '" + requalifDate + "', statutTIV = '" + status + "' where idEquipment = " + idEquipment 
+		print("Mise à jour des infos du bloc ayant pour numéro de serie: " + numeroSerie)
+		cursor = connection.cursor() 
+		# Exécution sql
+		cursor.execute(sql)  
+		# Commit
+		connection.commit()    
+		print("Database mise à jour pour le bloc n°" + numeroSerie)
 
 	
